42f99f394a064c46d4f86d251b47e061deaff9c8
Abdi Awale <abdi.awale5@gmail.com>
2026-07-24 19:24:24 +0000

    chore(alembic): update env.py to reflect new models path

--- alembic/env.py
+++ alembic/env.py
@@ -25,7 +25,7 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 # Import your SQLAlchemy Base
-from app.db.models import Base
+from app.models.accounts import Base
 target_metadata = Base.metadata
 
 
