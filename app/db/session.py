0ec66ffede2d9e70eda9ec2907bdd7c9db2f04a2
Abdi Awale <abdi.awale5@gmail.com>
2026-07-25 11:17:41 +0000

    feat(db): implement SQLAlchemy session setup and get_db dependency

---
+++ app/db/session.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create engine
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,        # avoids stale connections
+    echo=False                 # set True for debugging SQL
+)
+# Create session factory
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+# Dependency for FastAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield
+    finally:
+        db.close()    
