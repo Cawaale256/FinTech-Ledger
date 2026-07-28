cf688d117b4fb7e1533967537f209202403e546f
Abdi Awale <abdi.awale5@gmail.com>
2026-07-28 21:10:34 +0000

    chore(api): add router, models, schemas, and DB session imports to transfers module

--- app/api/transfers.py
+++ app/api/transfers.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends,status,HTTPException
+from app.models.transfers import Transfer
+from app.models.accounts import Account
+from app.schemas.transfers import TransferCreate, TransferResponse
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+
 
 router = APIRouter( tags=["Transfers"])
 
