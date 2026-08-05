ddf863113eafd3328083d508ca9e870c3276e42d
Abdi Awale <abdi.awale5@gmail.com>
2026-08-05 17:53:15 +0000

    refactor(core/security): replace os.getenv with settings from config module

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,3 @@
-# app/core/security.py
-
 from datetime import datetime, timedelta, timezone
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -9,14 +7,15 @@ from passlib.context import CryptContext
 from dotenv import load_dotenv
 import os
 
+from app.core.config import settings
 from app.db.session import get_db
 from app.models.users import User
 
-load_dotenv()
+# Load settings from config.py
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 # Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
