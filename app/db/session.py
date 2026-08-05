ea97ea0715bc98d1876b986cde638923634905a5
Abdi Awale <abdi.awale5@gmail.com>
2026-08-05 18:08:30 +0000

    refactor(db): migrate session engine to use config-based DATABASE_URL

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+# 
+from app.core.config import settings
+
+DATABASE_URL = settings.DATABASE_URL
 
 # Create engine
 engine = create_engine(
